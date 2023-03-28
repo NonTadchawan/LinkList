edcd8ed768e621b6d0c3eaa055a058b79b3e545f
non <tadchawan@geniustree.co.th>
2023-03-28 20:56:48 +0700

    create method contain and iterator

--- src/main/java/org/example/List.java
+++ src/main/java/org/example/List.java
@@ -1,16 +1,31 @@
 package org.example;
 
-public class List {
+import java.util.Iterator;
+
+public class List implements Iterable {
     Node head;
     Node tail;
-    public void add(Object value){
-        if(head==null){
-            head=new Node(value);
-            tail=head;
-        }else {
-           tail.next=new Node(value);
-            tail=tail.next;
+
+    public void add(Object value) {
+        if (head == null) {
+            head = new Node(value);
+            tail = head;
+        } else {
+            tail.next = new Node(value);
+            tail = tail.next;
         }
+    }
+
+    public boolean contain(Object value) {
+        this.tail = this.head;
+        System.out.println("Value: " + value);
+        while (tail != null) {
+            if (tail.value.equals(value)) {
+                return true;
+            }
+            tail = tail.next;
+        }
+        return false;
     }
 
     @Override
@@ -19,5 +34,27 @@ public class List {
                 "head=" + head +
                 ", tail=" + tail +
                 '}';
+    }
+
+    @Override
+    public Iterator iterator() {
+        this.tail = this.head;
+        return new Iterator() {
+            @Override
+            public boolean hasNext() {
+                return tail != null;
+            }
+
+            @Override
+            public Object next() {
+                Node current = tail;
+                tail = tail.next;
+                return current.value;
+            }
+
+            @Override
+            public void remove() {
+            }
+        };
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,18 @@
 package org.example;
-public class Main
-{
-    public static void main( String[] args ) {
-        List myList= new List();
+
+public class Main {
+    public static void main(String[] args) {
+        List myList = new List();
         myList.add("A");
         myList.add("B");
         myList.add("C");
         myList.add("D");
         System.out.println(myList);
+        System.out.println("Contain in myList: " + myList.contain("D"));
+        System.out.println("Contain in myList: " + myList.contain("F"));
+        System.out.print("Item in myList: ");
+        for (Object item : myList) {
+            System.out.print(item + " ");
+        }
     }
 }
